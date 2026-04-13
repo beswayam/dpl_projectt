97bf70f7dd7018b4203b7bf976fa65e1d627769c
chrisAmbagtsheer <chris.ambagtsheer@wur.nl>
2026-04-13 15:47:11 +0200

    CA WD: test case for ProteinStatisticsTest is fixed

--- BlastImplementation/src/tests/ProteinStatisticsTest.java
+++ BlastImplementation/src/tests/ProteinStatisticsTest.java
@@ -2,10 +2,12 @@ package tests;
 
 import junit.framework.TestCase;
 import utilities.ProteinStatistics;
+import utilities.Sequence;
 
 public class ProteinStatisticsTest extends TestCase {
 	String initialSeq = "ACDEFGHIKL";
-	ProteinStatistics seq = new ProteinStatistics(initialSeq);
+	Sequence protSeq = new Sequence(initialSeq);
+	ProteinStatistics seq = new ProteinStatistics(protSeq);
 	public void testMolecularWeightt() {		
 		double protWeight = seq.ProteinWeight();
 		assertEquals(protWeight, 1294.42);
