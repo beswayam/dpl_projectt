a6d73971e61f9a03a20223edd5627f9a3d482013
Rick Markus <Rick.markus@wur.nl>
2026-04-16 15:18:22 +0200

    WD RM: overload display method in Class Time.
    We overloaded the display method in Class Time as display() is needed for the interface,
    while display(Instant previousTime) is needed to test the display() method.

--- BlastImplementation/src/gui/MainGui.java
+++ BlastImplementation/src/gui/MainGui.java
@@ -269,7 +269,7 @@ public class MainGui extends JFrame {
         Instant startTime = time.getStartTime();
         Timer timer = new Timer(1000, new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-        		lblShowAppTime.setText(time.display(startTime));
+        		lblShowAppTime.setText(time.display());
         	}
         });
         timer.start();
