01ed9e3655380d85b7f2045b03970b05fc753b6d
Rick Markus <Rick.markus@wur.nl>
2026-04-17 15:57:39 +0200

    RM: add Javadoc comments to Class Time.

--- BlastImplementation/src/utilities/Time.java
+++ BlastImplementation/src/utilities/Time.java
@@ -3,20 +3,49 @@ package utilities;
 import java.time.Duration;
 import java.time.Instant;
 
-public class Time implements Displayable {
 
+/**
+ * The Time class represent a simple timer that tracks the elapsed time 
+ * since its instantiation.
+ * 
+ * It provides formatted output of the elapsed time in hours, minutes
+ * and seconds.
+ */
+public class Time implements Displayable {
+	
+	/** The moment this Time object was created. */
 	private final Instant startTime;
 
-	// Initiate with startTime
+	/** 
+	 * Constructs a Time object initialized to the current time. 
+	 */
 	public Time() {
 		this.startTime = Instant.now();
 	}
-
+	
+	/** 
+	 * Returns the start time of this timer.
+	 * 
+	 * @return the {@link Instant} when this object was created
+	 */
 	public Instant getStartTime() {
 		return this.startTime;
 	}
 
-	// Returns the time elapsed since instantiation of the object
+	/**
+	 * Returns a formatted string representing the time elapsed since
+	 * this object was created.
+	 * 
+	 * <p>
+	 * Format:
+	 * <ul>
+	 * 	<li>hh:mm:ss if hours are present</li>
+	 * <li>mm:ss if minutes are present</li>
+	 * <li>ss if seconds are present</li>
+	 * </ul>
+	 * 
+	 * @return a formatted string of elapsed time
+	 */
 	@Override
 	public String display() {
 		Duration elapsed = Duration.between(startTime, Instant.now());
@@ -37,8 +66,13 @@ public class Time implements Displayable {
 		return output;
 	}
 
-	// Returns the time elapsed between a given Instant object and the current time
-	// This method is only used to test the display() method
+	/** Returns a formatted string representing the time elapsed between 
+	 * a given {@link Instant} and the current time.
+	 * 
+	 * <p><b>Note:</b> This method is intended for testing purposes.
+	 * @param previousTime
+	 * @return a formatted string of elapsed time
+	 */
 	public String display(Instant previousTime) {
 		Duration elapsed = Duration.between(previousTime, Instant.now());
 
@@ -57,7 +91,12 @@ public class Time implements Displayable {
 		}
 		return output;
 	}
-
+	
+	/**
+	 * Indicates whether the displayed time needs to be updated.
+	 * 
+	 * @return always {@code true}
+	 */
 	public boolean needsUpdate() {
 		return true;
 	}
